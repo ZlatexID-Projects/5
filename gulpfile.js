bebf53a444be7337e4d79ba7b70c42b9cccf4bf6
Zlatex <zlatexyt@gmail.com>
2020-09-28 09:53:55 +0300

    а шо а нічо

--- gulpfile.js
+++ gulpfile.js
@@ -7,22 +7,24 @@ var inject = require('gulp-inject');
 var rename = require("gulp-rename");
 var cssmin = require('gulp-cssmin');
 
+// check changes in files
 gulp.task('watch', function(){
   browserSync.init({
     server: "./app"
 });
-  
   gulp.watch('app/scss/*.scss', gulp.series('sass')); 
   gulp.watch("app/*_.html", gulp.series('html'));
 })
 
-gulp.task('sass', function(){
-  
+// sass to css
+gulp.task('sass', function(){  
   return gulp.src('app/scss/**/*.scss')
     .pipe(sass()) // Using gulp-sass
     .pipe(gulp.dest('app/css'))
     .pipe(browserSync.stream())
 });
+
+//html inject scripts and styles and reload page
 gulp.task('html', function(){
   return gulp.src('app/*_.html')
     .pipe(rename("index.html"))
@@ -31,6 +33,7 @@ gulp.task('html', function(){
     .pipe(browserSync.stream())
 });
 
+//css min
 gulp.task('cssmin', function () {
     return gulp.src('app/scss/**/*.scss')
         .pipe(sass({
@@ -40,11 +43,14 @@ gulp.task('cssmin', function () {
         .pipe(gulp.dest('./build/css'));
 });
 
+//js min
 gulp.task('jsmin', function () {
   return gulp.src('app/js/**/*.js')
       .pipe(jsmin())
       .pipe(gulp.dest('./build/js'));
 });
+
+//html min
 gulp.task('htmlmin', function() {
   return gulp.src('app/index.html')
     .pipe(htmlmin({ collapseWhitespace: true }))
