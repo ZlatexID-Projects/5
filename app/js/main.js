07976b3ad9c750eab6d43fd79b3b314150107c16
Zlatex <zlatexyt@gmail.com>
2020-10-06 17:16:02 +0300

    плавная прокрутка

--- app/js/main.js
+++ app/js/main.js
@@ -33,3 +33,19 @@
 
     }
   });
+
+
+const anchors = document.querySelectorAll('a.nav-link')
+console.log(anchors)
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href')
+    
+    document.querySelector(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
